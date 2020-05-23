189642072f5a53ea908ed6074d9f61f01e99647a
sjb-ch1mp <samuel.brookes@hotmail.com>
2020-05-23 15:47:16 +1000

    finalised gui - fixed header and footer and created scrollable gui

---
+++ js/util.js
@@ -0,0 +1,17 @@
+function padContent(){
+    let headerHeight = document.getElementById("banner").clientHeight +
+        document.getElementById("chatter_box").clientHeight;
+    let footerHeight = document.getElementById("footer").clientHeight;
+    let content = document.getElementById("drop_zone");
+
+    content.setAttribute("style", "padding-top: " + (headerHeight*1.05) + "px; padding-bottom: " + (footerHeight*1.15) + "px;");
+}
+
+function addTestText(){
+    let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    let results = document.getElementById("results");
+    for(let i = 0; i<5; i++){
+        loremIpsum += loremIpsum;
+    }
+    results.innerText = loremIpsum;
+}
