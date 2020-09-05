a7fe58acb113129e451f93e12985b472b19517c0
sjb-ch1mp <samuel.brookes@hotmail.com>
2020-09-06 01:08:33 +1000

    extracting unique <input> elements for forms

--- js/link_rippr.js
+++ js/link_rippr.js
@@ -55,76 +55,89 @@ function loadFile(fileInfo) {
 }
 
 function ripLinks(){
-    if(file !== null && file !== undefined){
+    try{
+        if(file !== null && file !== undefined){
 
-        let resultString = ""
-        let tokenizer = new DOMTokenizer(file.toString());
-        let parser = new Parser(tokenizer);
-        if(parser.hasIocs()){
-            if(parser.iocs["base"] != null){
-                resultString += "=== DOM CONTAINS BASE ELEMENT ===\n\n";
-                resultString += "1: " + parser.iocs["base"] + "\n\n";
-            }
-            if(parser.iocs["form"].length > 0){
-                resultString += "=== DOM CONTAINS FORM ELEMENTS ===\n\n";
-                for(let i=0; i<parser.iocs["form"].length; i++){
-                    resultString += (i + 1) + ": " + parser.iocs["form"][i].method.toUpperCase() + " to " + parser.iocs["form"][i].action + "\n";
+            let resultString = ""
+            let tokenizer = new DOMTokenizer(file.toString());
+
+            let parser = new Parser(tokenizer);
+            if(parser.hasIocs()){
+                if(parser.iocs["base"] != null){
+                    resultString += "=== DOM CONTAINS BASE ELEMENT ===\n\n";
+                    resultString += "1: " + parser.iocs["base"] + "\n\n";
                 }
-                resultString += "\n";
-            }
-            if(parser.iocs["iframe"].length > 0){
-                resultString += "=== DOM CONTAINS IFRAMES ===\n\n";
-                for(let i=0; i<parser.iocs["iframe"].length; i++){
-                    resultString += (i + 1) + ": " + parser.iocs["iframe"][i] + "\n";
+                if(parser.iocs["form"].length > 0){
+                    resultString += "=== DOM CONTAINS FORM ELEMENTS ===\n\n";
+                    for(let i=0; i<parser.iocs["form"].length; i++){
+                        resultString += (i + 1) + ": " + parser.iocs["form"][i].method.toUpperCase() + " to " + parser.iocs["form"][i].action + "\n";
+                        if(parser.iocs["form"][i].inputs.length > 0){
+                            for(let j=0; j<parser.iocs["form"][i].inputs.length; j++){
+                                let input = parser.iocs["form"][i].inputs[j];
+                                resultString += "--> " + ((input.name === "")?"unnamed":"\"" + input.name + "\"") + " (" + ((input.type === "")?"untyped":input.type) + ")\n"
+                            }
+                            resultString += "\n";
+                        }
+                    }
+                    resultString += "\n";
                 }
-                resultString += "\n";
-            }
-            if(parser.iocs["a"].length > 0){
-                resultString += "=== DOM CONTAINS HYPERLINKS ===\n\n";
-                for(let i=0; i<parser.iocs["a"].length; i++){
-                    resultString += (i + 1) + ": " + parser.iocs["a"][i] + "\n";
+                if(parser.iocs["iframe"].length > 0){
+                    resultString += "=== DOM CONTAINS IFRAMES ===\n\n";
+                    for(let i=0; i<parser.iocs["iframe"].length; i++){
+                        resultString += (i + 1) + ": " + parser.iocs["iframe"][i] + "\n";
+                    }
+                    resultString += "\n";
                 }
-                resultString += "\n";
-            }
-            if(parser.iocs["script"].length > 0){
-                resultString += "=== DOM REFERENCES SCRIPTS ===\n\n";
-                for(let i=0; i<parser.iocs["script"].length; i++){
-                    resultString += (i + 1) + ": " + parser.iocs["script"][i] + "\n";
+                if(parser.iocs["a"].length > 0){
+                    resultString += "=== DOM CONTAINS HYPERLINKS ===\n\n";
+                    for(let i=0; i<parser.iocs["a"].length; i++){
+                        resultString += (i + 1) + ": " + parser.iocs["a"][i] + "\n";
+                    }
+                    resultString += "\n";
                 }
-                resultString += "\n";
+                if(parser.iocs["script"].length > 0){
+                    resultString += "=== DOM REFERENCES SCRIPTS ===\n\n";
+                    for(let i=0; i<parser.iocs["script"].length; i++){
+                        resultString += (i + 1) + ": " + parser.iocs["script"][i] + "\n";
+                    }
+                    resultString += "\n";
+                }
+            }else{
+                resultString = "=== NOTHING OF INTEREST IN DOM ===";
             }
+            /* DEBUGGING PARSER
+            let scripts = [];
+            while(tokenizer.hasNext()){
+                if(tokenizer.current.tokenType === DOMTokenType.SCRIPT){
+                    scripts.push(tokenizer.current.value);
+                }
+                resultString += "\n" + tokenizer.current.tokenType + ": " + tokenizer.current.value;
+                tokenizer.next();
+            }
+
+            if(scripts.length > 0){
+                resultString += "\n\n== TOKENIZED SCRIPTS ==";
+                for(let i=0; i<scripts.length; i++){
+                    let sTokenizer = new ScriptTokenizer(scripts[i]);
+                    resultString += "\n\n ++ SCRIPT " + (i+1) + ": ";
+                    while(sTokenizer.hasNext()){
+                        resultString += "\n" + sTokenizer.current.tokenType + ": " + sTokenizer.current.value;
+                        sTokenizer.next();
+                    }
+                }
+            }
+            */
+            results.innerText = resultString;
+            padContent();
+            chatter("Done");
         }else{
-            resultString = "=== NOTHING OF INTEREST IN DOM ===";
+            throwError("Error importing file");
         }
-        /* DEBUGGING PARSER
-        let scripts = [];
-        while(tokenizer.hasNext()){
-        	if(tokenizer.current.tokenType === DOMTokenType.SCRIPT){
-        		scripts.push(tokenizer.current.value);
-        	}
-            resultString += "\n" + tokenizer.current.tokenType + ": " + tokenizer.current.value;
-            tokenizer.next();
-        }
-        
-        if(scripts.length > 0){
-            resultString += "\n\n== TOKENIZED SCRIPTS ==";
-			for(let i=0; i<scripts.length; i++){
-				let sTokenizer = new ScriptTokenizer(scripts[i]);
-                resultString += "\n\n ++ SCRIPT " + (i+1) + ": ";
-				while(sTokenizer.hasNext()){
-                    resultString += "\n" + sTokenizer.current.tokenType + ": " + sTokenizer.current.value;
-					sTokenizer.next();
-				}
-			}
-        }
-        */
-        results.innerText = resultString;
+    }catch(err){
+        throwError("There was an error");
+        results.innerText = err.message.toUpperCase() + "\n\n" + err.stack;
         padContent();
-        chatter("Done");
-    }else{
-        throwError("Error importing file");
     }
-
 }
 
 function throwError(message){
--- js/parser.js
+++ js/parser.js
@@ -39,7 +39,10 @@ class Parser{
                         this.iocs["iframe"].push(this.getAttribute("href"));
                         break;
                     case "form":
-                        this.iocs["form"].push(this.parseForm());
+                        let form = this.parseForm();
+                        if(!form.alreadyParsed(this.iocs["form"])){
+                            this.iocs["form"].push(form);
+                        }
                         break;
                     case "script":
                         this.parseScript();
@@ -68,20 +71,38 @@ class Parser{
 
     parseForm(){
         let form = new Form("", "");
-        while(this.domTokenizer.hasNext() && this.domTokenizer.current.tokenType !== DOMTokenType.CLOSE_TAG_START){
+        //get form attributes
+        while(this.domTokenizer.hasNext() && this.domTokenizer.current.tokenType !== DOMTokenType.DEFAULT_TAG_FINISH){
             if(this.domTokenizer.current.tokenType === DOMTokenType.ATT_KEY){
                 if(this.domTokenizer.current.value === "method" || this.domTokenizer.current.value === "action"){
                     let att_key = this.domTokenizer.current.value;
-                    while(this.domTokenizer.current.tokenType !== DOMTokenType.ATT_VALUE){
-                        this.domTokenizer.next();
-                    }
+                    this.domTokenizer.next(); //EQUALS
+                    this.domTokenizer.next(); //QUOTE
+                    this.domTokenizer.next(); //ATT_VALUE or QUOTE if empty
                     switch(att_key){
                         case "method":
-                            form.method = this.domTokenizer.current.value;
+                            form.method = (this.domTokenizer.current.tokenType === DOMTokenType.QUOTE) ? "" : this.domTokenizer.current.value;
                             break;
                         default:
-                            form.action = this.domTokenizer.current.value;
+                            form.action = (this.domTokenizer.current.tokenType === DOMTokenType.QUOTE) ? "/" : this.domTokenizer.current.value;
                     }
+                }
+            }
+            this.domTokenizer.next();
+        }
+
+        //get form inputs
+        while(this.domTokenizer.hasNext() &&
+            !(this.domTokenizer.current.tokenType === DOMTokenType.CLOSE_TAG_NAME &&
+                this.domTokenizer.current.value === "form")){
+            if(this.domTokenizer.current.tokenType === DOMTokenType.OPEN_TAG_NAME &&
+                this.domTokenizer.current.value === "input"){
+                let inputAttributes = this.getAttribute("*");
+                if("name" in inputAttributes || "type" in inputAttributes){
+                    form.inputs.push(new Input(
+                        ("name" in inputAttributes) ? inputAttributes["name"]:"",
+                        ("type" in inputAttributes) ? inputAttributes["type"]:""
+                    ));
                 }
             }
             this.domTokenizer.next();
@@ -93,41 +114,46 @@ class Parser{
         if(attributeName === "*"){ //get all attributes
             let attributes = null;
             while(this.domTokenizer.hasNext() &&
-                this.domTokenizer.current.tokenType !== DOMTokenType.CLOSE_TAG_START &&
+                this.domTokenizer.current.tokenType !== DOMTokenType.DEFAULT_TAG_FINISH &&
                 this.domTokenizer.current.tokenType !== DOMTokenType.VOID_TAG_FINISH){
                 if(this.domTokenizer.current.tokenType === DOMTokenType.ATT_KEY){
                     if(attributes == null){
                         attributes = {};
                     }
                     let att_key = this.domTokenizer.current.value;
-                    this.domTokenizer.next();
-                    if(this.domTokenizer.current.tokenType === DOMTokenType.EQUALS){
-                        while(this.domTokenizer.current.tokenType != DOMTokenType.ATT_VALUE){
-                            this.domTokenizer.next();
-                        }
-                        attributes[att_key] = this.domTokenizer.current.value;
+                    this.domTokenizer.next(); //EQUALS
+                    this.domTokenizer.next(); //QUOTE
+                    this.domTokenizer.next(); //QUOTE or ATT_VALUE
+                    if(this.domTokenizer.current.tokenType === DOMTokenType.QUOTE){
+                        attributes[att_key] = "";
                     }else{
-                        attributes[att_key] = "True";
+                        attributes[att_key] = this.domTokenizer.current.value;
                     }
+                }else if(this.domTokenizer.current.tokenType === DOMTokenType.BOOL_ATT){
+                    if(attributes == null){
+                        attributes = {};
+                    }
+                    attributes[this.domTokenizer.current.value] = "True";
                 }
                 this.domTokenizer.next();
             }
             return attributes;
         }else{
             while(this.domTokenizer.hasNext() &&
-                this.domTokenizer.current.tokenType !== DOMTokenType.CLOSE_TAG_START &&
+                this.domTokenizer.current.tokenType !== DOMTokenType.DEFAULT_TAG_FINISH &&
                 this.domTokenizer.current.tokenType !== DOMTokenType.VOID_TAG_FINISH){
                 if(this.domTokenizer.current.tokenType === DOMTokenType.ATT_KEY &&
                     this.domTokenizer.current.value === attributeName){
-                    this.domTokenizer.next();
-                    if(this.domTokenizer.current.tokenType === DOMTokenType.EQUALS){
-                        while(this.domTokenizer.current.tokenType != DOMTokenType.ATT_VALUE){
-                            this.domTokenizer.next();
-                        }
-                        return this.domTokenizer.current.value;
+                    this.domTokenizer.next(); //EQUALS
+                    this.domTokenizer.next(); //QUOTE
+                    this.domTokenizer.next(); //QUOTE or ATT_VALUE
+                    if(this.domTokenizer.current.tokenType === DOMTokenType.QUOTE){
+                        return "";
                     }else{
-                        return "True";
+                        return this.domTokenizer.current.value;
                     }
+                }else if(this.domTokenizer.current.tokenType === DOMTokenType.BOOL_ATT){
+                    return "True";
                 }
                 this.domTokenizer.next();
             }
@@ -139,5 +165,42 @@ class Form{
     constructor(method, action){
         this.method = method;
         this.action = action;
+        this.inputs = [];
+    }
+
+    equals(form){
+        if(this.method !== form.method) return false;
+        if(this.action !== form.action) return false;
+        for(let i=0; i<this.inputs.length; i++){
+            if(!this.inputs[i].existsInArray(form.inputs)) return false;
+        }
+        return true;
+    }
+
+    alreadyParsed(formList){
+        for(let i=0; i<formList.length; i++){
+            if(formList[i].equals(this)) return true;
+        }
+        return false;
+    }
+}
+
+class Input{
+    constructor(name, type){
+        this.name = name;
+        this.type = type;
+    }
+
+    equals(input){
+        if(this.name !== input.name) return false;
+        if(this.type !== input.type) return false;
+        return true;
+    }
+
+    existsInArray(inputList){
+        for(let i=0; i<inputList.length; i++){
+            if(inputList[i].equals(this)) return true;
+        }
+        return false;
     }
 }
