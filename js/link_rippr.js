5f727cab61c069af6616aa3c9f6f72cdd5aa5e19
sjb-ch1mp <samuel.brookes@hotmail.com>
2020-09-07 22:01:01 +1000

    changed chatter

--- js/link_rippr.js
+++ js/link_rippr.js
@@ -8,6 +8,7 @@ let defaultTags = null;
 function setUpGlobalVariables(){
 
     chatterBox = document.getElementById("chatter_box");
+    chatter("Drop an HTML file");
     results = document.getElementById("results");
     defaultTags = ["script", "a", "base", "iframe", "form", "input"];
 
